e0a174c47aec6e5692abb2a390ffa19b859f77db
René Rendón <rene.d.rendon@unitec.edu>
2018-12-12 20:28:32 -0600

    Fixed rebase issues

--- src/actions/General.js
+++ src/actions/General.js
@@ -1,12 +1,22 @@
 import {
-    CONSTANT_VALUE,
-    LIST_PROFILES
+	CONSTANT_VALUE,
+	LIST_PROFILES,
+	CREATE_PROFILE,
+	CREATE_PROFILE_SUCEDED
 } from 'constants/ActionTypes';
 
 export function setNewValue(newValue) {
-    return {type: CONSTANT_VALUE, newValue};
+	return { type: CONSTANT_VALUE, newValue };
+}
+
+export function createProfile(profile) {
+	return { type: CREATE_PROFILE, profile };
+}
+
+export function setCreateProfileSuceded(value) {
+	return { type: CREATE_PROFILE_SUCEDED, createProfileSuceded: value };
 }
 
 export function listProfiles() {
-    return {type: LIST_PROFILES};
+	return { type: LIST_PROFILES };
 }
